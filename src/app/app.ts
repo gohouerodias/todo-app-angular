b18eb80c64e10edafa71ff4618f95e7432d1a69e
gohouerodias <gohouerodias@gmail.com>
2026-01-14 22:47:35 +0800

    init

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,15 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { SideBar } from './side-bar/side-bar';
+import { Main } from './main/main';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     Header,
-    SideBar],
+    SideBar,
+    Main],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
